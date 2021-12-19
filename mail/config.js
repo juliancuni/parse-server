3e7177e2d3e11c6a9c545370617ecfa77e1fb3b5
Julian Cuni <julian.cuni@microservices.al>
2021-12-19 16:05:17 +0100

    Mail Template

--- mail/config.js
+++ mail/config.js
@@ -7,7 +7,24 @@ const mailAdapterConfig = {
         port: process.env.MAIL_PORT || 587,
         user: process.env.MAIL_USER || "name@domain.com",
         password: process.env.MAIL_PASS || "SecurePassword",
-        fromAddress: process.env.MAIL_FROM_ADDRESS || 'app@domain.com'
+        fromAddress: process.env.MAIL_FROM_ADDRESS || 'app@domain.com',
+        template: true,
+        templatePath: "mail/templates/mail.html",
+        passwordOptions: {
+            subject: "Password recovery",
+            body: "Custome pasword recovery email body",
+            btn: "Recover your password"
+            /* --EXTRA PARAMETERS--
+            others: {
+              extraParameter
+            }
+            */
+        },
+        confirmOptions: {
+            subject: "E-mail confirmation",
+            body: "Custome email confirmation body",
+            btn: "confirm your email"
+        },
     }
 }
 
