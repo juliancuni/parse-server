d598b7c9389d2b59c2074032b411d57bfd425799
Julian Cuni <julian.cuni@microservices.al>
2021-12-19 19:10:30 +0100

    cleanup index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// Example express application adding the parse-server module to expose Parse
-// compatible API routes.
 const mailAdapterConfig = require('./mail/config.js');
 const express = require('express');
 const ParseServer = require('parse-server').ParseServer;
@@ -17,59 +15,43 @@ const config = {
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   appName: process.env.APP_NAME || "MSA",
-  masterKey: process.env.MASTER_KEY || 'masterKey', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/dev', // Don't forget to change to https if needed
+  masterKey: process.env.MASTER_KEY || 'masterKey',
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/dev',
   publicServerURL: process.env.PUBLIC_SERVER_URL || "",
   allowClientClassCreation: process.env.CLIENT_CLASS_CREATION || false,
   verifyUserEmails: process.env.VERIFY_USERS_EMAILS || true,
 
   emailAdapter: mailAdapterConfig,
   accountLockout: {
-    // Lock the account for 5 minutes.
-    duration: 5,
-    // Lock an account after 3 failed log-in attempts
-    threshold: 3,
-    // Unlock the account after a successful password reset
+    duration: 3,
+    threshold: 5,
     unlockOnPasswordReset: true,
   },
-  // The password policy
   passwordPolicy: {
-    // Enforce a password of at least 8 characters which contain at least 1 lower case, 1 upper case and 1 digit
     validatorPattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/,
-    // Do not allow the username as part of the password
     doNotAllowUsername: true,
-    // Do not allow to re-use the last 5 passwords when setting a new password
     maxPasswordHistory: 5,
   },
-
   // liveQuery: {
   //   classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
   // },
+  clientKey: process.env.CLIENT_KEY || "",
+  dotNetKey: process.env.DOTNET_KEY || "",
+  restAPIKey: process.env.RESTAPI_KEY || "",
   javascriptKey: process.env.JS_KEY || "",
 };
-// Client-keys like the javascript key or the .NET key are not necessary with parse-server
-// If you wish you require them, you can set them as options in the initialization above:
-// javascriptKey, restAPIKey, dotNetKey, clientKey
 
 const app = express();
-
-// Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
-
-// Serve the Parse API on the /dev URL prefix
 const mountPath = process.env.PARSE_MOUNT || '/dev';
 if (!test) {
   const api = new ParseServer(config);
   app.use(mountPath, api);
 }
 
-// Parse Server plays nicely with the rest of your web routes
 app.get('/', function (req, res) {
   res.status(200).send('DEV-SERVER');
 });
-
-// There will be a test page available on the /test path of your server url
-// Remove this before launching your app
 // app.get('/test', function (req, res) {
 //   res.sendFile(path.join(__dirname, '/public/test.html'));
 // });
@@ -80,7 +62,6 @@ if (!test) {
   httpServer.listen(port, function () {
     console.log('parse-server-development running on port ' + port + '.');
   });
-  // This will enable the Live Query real-time server
   // ParseServer.createLiveQueryServer(httpServer);
 }
 
