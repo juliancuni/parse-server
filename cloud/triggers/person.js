52ddf0de2b8909cf97e2f1be6c051eec4989aa87
Julian Cuni <julian.cuni@microservices.al>
2021-12-25 14:24:05 +0100

    Cloud again. Not validating...

--- cloud/triggers/person.js
+++ cloud/triggers/person.js
@@ -1,30 +1,30 @@
 const personClass = "Person";
 const personUniqueKey = "nid";
 //Check person unique nid;
-Parse.Cloud.beforeSave(personClass, function(request, response) {
+Parse.Cloud.beforeSave(personClass, function (request, response) {
     if (!request.object.isNew()) {
-       // Let Personi Egziston updates go through
-       response.success();
+        // Let Personi Egziston updates go through
+        response.success();
     }
     var query = new Parse.Query(personClass);
     // Add query filters to check for uniqueness
     query.equalTo(personUniqueKey, request.object.get(personUniqueKey));
-    query.first().then(function(existingObject) {
-       if (existingObject) {
-          response.error("Personi Egziston");
-       } else {
-          // Pass a flag that this is not an Personi Egziston
-          return Parse.Promise.as(false);
-       }
-    }).then(function(existingObject) {
-       if (existingObject) {
-          // Personi Egziston, stop initial save
-          response.error("Personi Egziston");
-       } else {
-          // New object, let the save go through
-          response.success();
-       }
-    }, function(error) {
-       response.error(error);
+    query.first().then(function (existingObject) {
+        if (existingObject) {
+            response.error("Personi Egziston");
+        } else {
+            // Pass a flag that this is not an Personi Egziston
+            return Parse.Promise.as(false);
+        }
+    }).then(function (existingObject) {
+        if (existingObject) {
+            // Personi Egziston, stop initial save
+            response.error("Personi Egziston");
+        } else {
+            // New object, let the save go through
+            response.success();
+        }
+    }).catch((err) => {
+        response.error(err);
     });
- });
+});
