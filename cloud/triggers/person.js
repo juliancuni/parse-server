d3355dad9e3ca8517eecc74ad43a48b658c0a027
Julian Cuni <julian.cuni@microservices.al>
2021-12-25 14:02:30 +0100

    Cloud testing

--- cloud/triggers/person.js
+++ cloud/triggers/person.js
@@ -2,24 +2,24 @@ const personClass = "Person";
 const personUniqueKey = "nid";
 //Check person unique nid;
 Parse.Cloud.beforeSave(personClass, function (request, response) {
-  var myObject = request.object;
-
-  if (myObject.isNew()) {
-    var query = new Parse.Query(personClass);
-    query.equalTo(personUniqueKey, myObject.get(personUniqueKey));
-    query.count({
-      success: function (number) {
-        if (number > 0) {
-          response.error("Personi egziston");
-        } else {
-          response.success();
-        }
-      },
-      error: function (error) {
+    var myObject = request.object;
+    console.log(myObject);
+    if (myObject.isNew()) {
+        var query = new Parse.Query(personClass);
+        query.equalTo(personUniqueKey, myObject.get(personUniqueKey));
+        query.count({
+            success: function (number) {
+                if (number > 0) {
+                    response.error("Personi egziston");
+                } else {
+                    response.success();
+                }
+            },
+            error: function (error) {
+                response.success();
+            }
+        })
+    } else {
         response.success();
-      }
-    })
-  } else {
-    response.success();
-  }
+    }
 });
