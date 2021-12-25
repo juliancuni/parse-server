86eb549dd49a211061a86614e64bbe2574ea0d1e
Julian Cuni <julian.cuni@microservices.al>
2021-12-25 16:27:50 +0100

    Cloud code beforeSave Person done!

--- cloud/triggers/person.js
+++ cloud/triggers/person.js
@@ -10,7 +10,7 @@ Parse.Cloud.beforeSave(personClass, async (request) => {
         const exists = await query.first({ useMasterKey: true })
         if (exists) {
             console.log("Egzistonnnnnnnnnnnnnnn");
-            throw new Parse.Error(101, 'Personi egziston');
+            throw new Parse.Error(1001, 'Personi egziston');
         }
     }
 });
@@ -18,27 +18,3 @@ Parse.Cloud.beforeSave(personClass, async (request) => {
 
 
 // E15414075G
-
-
-
-
-// query.first().then((existingObject) => {
-//     if (existingObject) {
-//         console.log("Personi Egziston if query.first existingObject")
-//         response.error("Personi Egziston");
-//     } else {
-//         // Pass a flag that this is not an Personi Egziston
-//         console.log("Personi Nuk Egziston else query.first existingObject")
-//         return Parse.Promise.as(false);
-//     }
-// }).then((existingObject) => {
-//     if (existingObject) {
-//         console.log("Personi Egziston if then((existingObject)")
-//         // Personi Egziston, stop initial save
-//         response.error("Personi Egziston");
-//     } else {
-//         console.log("Personi Nuk Egziston else then((existingObject)")
-//         // New object, let the save go through
-//         response.success();
-//     }
-// })
