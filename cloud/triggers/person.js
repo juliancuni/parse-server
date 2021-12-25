64e462739d5a80b6d1d7c8e4583c41931ff7d89e
Julian Cuni <julian.cuni@microservices.al>
2021-12-25 14:58:40 +0100

    Cloud......

--- cloud/triggers/person.js
+++ cloud/triggers/person.js
@@ -2,35 +2,48 @@ const personClass = "Person";
 const personUniqueKey = "nid";
 //Check person unique nid;
 Parse.Cloud.beforeSave(personClass, (request, response) => {
-    if (!request.object.isNew()) {
-        // Let Personi Egziston updates go through
-        console.log("isNotNew")
+    const person = request.object;
+    console.log(person.isNew());
+    if (!person.isNew()) {
         response.success();
     }
-    var query = new Parse.Query(personClass);
-    // Add query filters to check for uniqueness
-    query.equalTo(personUniqueKey, request.object.get(personUniqueKey));
-    query.first().then((existingObject) => {
-        if (existingObject) {
-            console.log("Personi Egziston if query.first existingObject")
-            response.error("Personi Egziston");
-        } else {
-            // Pass a flag that this is not an Personi Egziston
-            console.log("Personi Nuk Egziston else query.first existingObject")
-            return Parse.Promise.as(false);
-        }
-    }).then((existingObject) => {
-        if (existingObject) {
-            console.log("Personi Egziston if then((existingObject)")
-            // Personi Egziston, stop initial save
-            response.error("Personi Egziston");
-        } else {
-            console.log("Personi Nuk Egziston else then((existingObject)")
-            // New object, let the save go through
+    const query = new Parse.Query(personClass);
+    query.equalTo(personUniqueKey, person.get(personUniqueKey));
+    query.first({
+        success: (obj) => {
+            if (obj) response.error("Personi Egziston");
+            response.success();
+        },
+        error: (err) => {
             response.success();
         }
     })
 });
 
 
+
 // E15414075G
+
+
+
+
+// query.first().then((existingObject) => {
+//     if (existingObject) {
+//         console.log("Personi Egziston if query.first existingObject")
+//         response.error("Personi Egziston");
+//     } else {
+//         // Pass a flag that this is not an Personi Egziston
+//         console.log("Personi Nuk Egziston else query.first existingObject")
+//         return Parse.Promise.as(false);
+//     }
+// }).then((existingObject) => {
+//     if (existingObject) {
+//         console.log("Personi Egziston if then((existingObject)")
+//         // Personi Egziston, stop initial save
+//         response.error("Personi Egziston");
+//     } else {
+//         console.log("Personi Nuk Egziston else then((existingObject)")
+//         // New object, let the save go through
+//         response.success();
+//     }
+// })
