96a3bb19e71d5f0c6ac35afb68798a4d8687747b
Julian Cuni <julian.cuni@microservices.al>
2021-12-25 14:42:56 +0100

    Cloud ...

--- cloud/triggers/person.js
+++ cloud/triggers/person.js
@@ -1,26 +1,31 @@
 const personClass = "Person";
 const personUniqueKey = "nid";
 //Check person unique nid;
-Parse.Cloud.beforeSave(personClass, function (request, response) {
+Parse.Cloud.beforeSave(personClass, (request, response) => {
     if (!request.object.isNew()) {
         // Let Personi Egziston updates go through
+        console.log("isNotNew")
         response.success();
     }
     var query = new Parse.Query(personClass);
     // Add query filters to check for uniqueness
     query.equalTo(personUniqueKey, request.object.get(personUniqueKey));
-    query.first().then(function (existingObject) {
+    query.first().then((existingObject) => {
         if (existingObject) {
+            console.log("Personi Egziston if query.first existingObject")
             response.error("Personi Egziston");
         } else {
             // Pass a flag that this is not an Personi Egziston
+            console.log("Personi Nuk Egziston else query.first existingObject")
             return Parse.Promise.as(false);
         }
-    }).then(function (existingObject) {
+    }).then((existingObject) => {
         if (existingObject) {
+            console.log("Personi Egziston if then((existingObject)")
             // Personi Egziston, stop initial save
             response.error("Personi Egziston");
         } else {
+            console.log("Personi Nuk Egziston else then((existingObject)")
             // New object, let the save go through
             response.success();
         }
